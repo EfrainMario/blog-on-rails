4bc1909eb8a6056529adb86b8422a7d2817f2251
efrainmario <efrainmario345@gmail.com>
2023-08-09 10:28:13 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
