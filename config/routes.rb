3b61a3d64f2a616a53253bada31f51ab84991a67
efrainmario <efrainmario345@gmail.com>
2023-08-13 15:46:31 +0100

    Add comment feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
